bbc3f658cad7fde4573b4b3d64925e781cf6b93f
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-10-20 12:57:26 +0800

    Create NonSquareMatrix.java

---
+++ src/Lecture_9/NonSquareMatrix.java
@@ -0,0 +1,11 @@
+package Lecture_9;
+
+public class NonSquareMatrix {
+    public static void main(String[] args){
+//        arr = {{1,3,4},{1,4},{1,4,6,7,3,2}}
+        int[][] arr = new int[3][];
+        arr[0] = new int[3];
+        arr[1] = new int[2];
+        arr[2] = new int[6];
+    }
+}
