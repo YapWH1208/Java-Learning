333d548d5c851d5218bb637030436109b86a905f
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-10-23 14:04:02 +0800

    Create Exercise2.java

---
+++ src/Lecture_10/Exercise2.java
@@ -0,0 +1,11 @@
+package Lecture_10;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args){
+        // TODO (YapWH 19/10/2023 10:46 AM) 2D Array as Input, find the row index of row with highest sum
+        Scanner scanner = new Scanner(System.in);
+        int
+    }
+}
