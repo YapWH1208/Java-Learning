90dd32c069c492abb1569cac635360bc8457aca6
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-10-25 10:35:43 +0800

    Update Exercise2.java

--- src/Lecture_10/Exercise2.java
+++ src/Lecture_10/Exercise2.java
@@ -4,8 +4,30 @@ import java.util.Scanner;
 
 public class Exercise2 {
     public static void main(String[] args){
-        // TODO (YapWH 19/10/2023 10:46 AM) 2D Array as Input, find the row index of row with highest sum
         Scanner scanner = new Scanner(System.in);
-        int
+        int[][] arr = new int[3][3];
+
+        for(int[] i:arr){
+            for(int j:i){
+                j = scanner.nextInt();
+            }
+        }
+
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int high = 0;
+
+        for(int i=0; i<arr.length; i++){
+            sum = 0;
+            for(int j=0; j<arr[i].length; j++){
+                sum += j;
+            }
+            if(sum>max){
+                max = sum;
+                high = i;
+            }
+        }
+
+        System.out.printf("The row with highest sum is %d with value of %d", high, max);
     }
 }
