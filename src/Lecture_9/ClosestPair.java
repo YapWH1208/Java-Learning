fdff204b1d603c4f707f8a3821796774d36af7bc
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-11-23 12:13:17 +0800

    Update ClosestPair.java

--- src/Lecture_9/ClosestPair.java
+++ src/Lecture_9/ClosestPair.java
@@ -5,10 +5,10 @@ public class ClosestPair {
         double[][] coord = {{-1,3},{-1,-1},{1,1},{2,0.5},{2,-1},{3,3},{4,2},{4,-0.5}};
         double min = Double.POSITIVE_INFINITY;
         double distance;
-        int c1=0,c2=0;
+        int c1=0, c2=0;
 
         for(int i=1; i< coord.length; i++){
-            for(int j=i+1; j<coord.length; j++){
+            for(int j=i+1; j<coord[i].length; j++){
                 distance = Math.sqrt(Math.pow(coord[i][0]-coord[j][0],2)+Math.pow(coord[i][1]-coord[j][1],2));
                 if(distance<min){
                     min = distance;
