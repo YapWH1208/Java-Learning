d3e9e836da1d0b75fca4d69bb6fc42b4f2fb00a1
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2023-12-19 08:27:08 +0800

    Create CourseMain.java

---
+++ src/Exercise/CourseMain.java
@@ -0,0 +1,11 @@
+// Created by YapWH On 14/12/2023 10:23 AM
+
+package Exercise;
+
+public class CourseMain {
+    public static void main(String[] args){
+        Course lecture = new Lecture(1, "Introduction to Java", 3, 2 );
+        System.out.println(lecture);
+        System.out.println();
+    }
+}
