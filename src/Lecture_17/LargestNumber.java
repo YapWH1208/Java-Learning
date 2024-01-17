bba7a3e668e3dd4421f78ce25da4117ace668fb0
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-01-17 21:09:13 +0800

    Create LargestNumber.java

---
+++ src/Lecture_17/LargestNumber.java
@@ -0,0 +1,32 @@
+// Created by YapWH On 14/1/2024 4:41 PM
+
+package Lecture_17;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        ArrayList<Number> list = new ArrayList<>();
+        list.add(45);
+        list.add(3445.53);
+        list.add(new BigInteger("3432323234344343101"));
+        list.add(new BigInteger("3432323234344343102"));
+
+        System.out.println("The largest number is " + getLargestNumber(list));
+    }
+
+    public static Number getLargestNumber(ArrayList<Number> list) {
+        if (list == null || list.size() == 0) {
+            return null;
+        }
+
+        Number number = list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            if (number.doubleValue() < list.get(i).doubleValue()) {
+                number = list.get(i);
+            }
+        }
+        return number;
+    }
+}
