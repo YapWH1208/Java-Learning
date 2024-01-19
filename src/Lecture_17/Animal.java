90a10490110e9ebbe62a0c00a591e3d95c06ed23
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-01-19 10:57:21 +0800

    Create Animal.java

---
+++ src/Lecture_17/Animal.java
@@ -0,0 +1,13 @@
+// Created by YapWH On 14/1/2024 4:48 PM
+
+package Lecture_17;
+
+public abstract class Animal {
+    private String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public abstract String sound();
+}
