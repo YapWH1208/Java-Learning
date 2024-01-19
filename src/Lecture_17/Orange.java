171d7a6e21e6d149aa02a6347e92804fd2c4cac6
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-01-19 10:57:32 +0800

    Create Orange.java

---
+++ src/Lecture_17/Orange.java
@@ -0,0 +1,10 @@
+// Created by YapWH On 14/1/2024 4:51 PM
+
+package Lecture_17;
+
+public class Orange extends Fruit{
+    @Override
+    public String howToEat(){
+        return "Orange: Make orange juice";
+    }
+}
