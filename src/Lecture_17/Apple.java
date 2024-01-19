dfa1c6ff1a7447e25a22fe973ba53be37286f5d9
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-01-19 10:57:30 +0800

    Create Apple.java

---
+++ src/Lecture_17/Apple.java
@@ -0,0 +1,10 @@
+// Created by YapWH On 14/1/2024 4:51 PM
+
+package Lecture_17;
+
+public class Apple extends Fruit{
+    @Override
+    public String howToEat(){
+        return "Apple: Make apple cider";
+    }
+}
