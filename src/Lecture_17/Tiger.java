a499160dd5dcf99aaa0937589ea23efc5332fb79
YapWH1208 <107160166+YapWH1208@users.noreply.github.com>
2024-01-19 10:57:26 +0800

    Create Tiger.java

---
+++ src/Lecture_17/Tiger.java
@@ -0,0 +1,14 @@
+// Created by YapWH On 14/1/2024 4:50 PM
+
+package Lecture_17;
+
+public class Tiger extends Animal{
+    public Tiger() {
+        super("Tiger");
+    }
+
+    @Override
+    public String sound(){
+        return "Tiger: RROOAARR";
+    }
+}
